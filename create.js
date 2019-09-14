8d7b78afb44f349128602cbe7d08425777d7c0c0
Anmol Desai <anmoldesai4@gmail.com>
2019-09-14 14:25:43 +1000

    setup crate

--- create.js
+++ create.js
@@ -1,6 +1,6 @@
-import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
+import shortid from 'shortid';
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
@@ -8,10 +8,23 @@ export async function main(event, context) {
     TableName: process.env.tableName,
     Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: uuid.v1(),
-      content: data.content,
-      attachment: data.attachment,
-      createdAt: Date.now()
+      id: shortid.generate(),
+      firstName: data.firstName,
+      lastName: data.lastName,
+      fatherName: data.fatherName,
+      motherName: data.motherName,
+      address: data.address,
+      fatherMobile: data.fatherMobile,
+      motherMobile: data.motherMobile,
+      isRegistered: false,
+      activities: [],
+      email: data.email,
+      age: data.age,
+      emergencyContact: data.emergencyContact,
+      emergencyNumber: data.emergencyNumber,
+      qr: data.qr,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
     }
   };
 
