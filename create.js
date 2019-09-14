43b3e9e294e63ce9863f92ab9097b4b0be70ba73
Anmol Desai <anmoldesai4@gmail.com>
2019-09-14 15:34:29 +1000

    try to upload to s3

--- create.js
+++ create.js
@@ -1,13 +1,31 @@
 import * as dynamoDbLib from "./libs/dynamodb-lib";
+import * as s3 from "./libs/s3-lib";
 import { success, failure } from "./libs/response-lib";
 import shortid from 'shortid';
+import Qr from 'qrcode';
+// import fs from 'fs';
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
+  const id = shortid.generate();
+  // const qrFileStream = QrCode.toFileStream(fs.createWriteStream(), [id, data.firstName, data.lastName, data.emergencyContact, data.emergencyNumber]);
+
+  try{
+    await s3.call("upload", {
+      Bucket: process.env.bucketName,
+      Key: `${id}.png`,
+      Body: new Buffer(Qr.toDataURL([id, data.firstName, data.lastName]).replace(/^data:image\/\w+;base64,/, ""),'base64'),
+      ContentEncoding: 'base64',
+      ContentType: 'image/png'
+    });
+  } catch (e) {
+    return failure({ status: false, message: 'Failure to upload qr' });
+  }
+
   const params = {
     TableName: process.env.tableName,
     Item: {
-      id: shortid.generate(),
+      id,
       firstName: data.firstName,
       lastName: data.lastName,
       fatherName: data.fatherName,
@@ -21,7 +39,7 @@ export async function main(event, context) {
       age: data.age,
       emergencyContact: data.emergencyContact,
       emergencyNumber: data.emergencyNumber,
-      qr: data.qr,
+      qr: `${id}.png`,
       createdAt: Date.now(),
       updatedAt: Date.now(),
     }
