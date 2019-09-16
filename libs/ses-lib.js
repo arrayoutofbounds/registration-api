d44c074252869ae0ac4ca991da754ee506794c3a
Anmol Desai <anmoldesai4@gmail.com>
2019-09-16 19:32:31 +1000

    updated to have logs and updated ses

--- libs/ses-lib.js
+++ libs/ses-lib.js
@@ -4,13 +4,11 @@ import nodemailer from 'nodemailer';
 export function call(item) {
     const ses = new AWS.SES({apiVersion: '2010-12-01', region: 'us-west-2'});
 
-    const message = `
-        <h1> Hello ${item.firstName} </h1>
+    const message = `<h1> Hello ${item.firstName} </h1>
         <p> BAPS is glad to present you with an invitation to the 2019 kids diwali festivel <p>
         <p> Please see the attached qr code below </p>
 
-        <p> Jai Swaminarayan </p>
-    `;
+        <p> Jai Swaminarayan </p>`;
 
     const mailOptions = {
         from: "anmoldesai4@gmail.com",
