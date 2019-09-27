50355f124ec83aa6c614c792fc3c6dac99858ddb
Anmol Desai <anmoldesai4@gmail.com>
2019-09-27 16:00:32 +1000

    add streams

---
+++ testing.js
@@ -0,0 +1,12 @@
+import { DynamoDB } from 'aws-sdk';
+
+const converter = DynamoDB.Converter;
+
+export async function main(event, context) {
+    event.Records.forEach(record => {
+        if(record.eventName === 'INSERT'){
+            const unMarshelledRecord = converter.unmarshall(record.dynamodb.NewImage);
+            console.log(unMarshelledRecord);
+        }  
+    });
+}
