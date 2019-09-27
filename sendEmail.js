40ce1a5e4fcdd59d5b2c9fc89ae043b61b90e5e0
Anmol Desai <anmoldesai4@gmail.com>
2019-09-27 17:25:21 +1000

    remove foreach as its not async

--- sendEmail.js
+++ sendEmail.js
@@ -4,10 +4,9 @@ import * as sesLib from './libs/ses-lib';
 const converter = DynamoDB.Converter;
 
 export async function main(event, context) {
-    event.Records.forEach(async record => {
+    for (let record in event.Records) {
         if (record.eventName === 'INSERT') {
             const unMarshelledRecord = converter.unmarshall(record.dynamodb.NewImage);
-
             // send an email using SES
             try {
                 console.log("sending email");
@@ -16,5 +15,5 @@ export async function main(event, context) {
                 console.log("sending email failed");
             }
         }
-    });
+    }
 }
