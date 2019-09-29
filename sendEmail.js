fe656b5e91b10611f684725a3f2abc7a8ce7dcbe
Anmol Desai <anmoldesai4@gmail.com>
2019-09-29 19:19:12 +1000

    make for loop

--- sendEmail.js
+++ sendEmail.js
@@ -5,7 +5,8 @@ const converter = DynamoDB.Converter;
 
 export async function main(event, context) {
     console.log(event.Records);
-    for (let record in event.Records) {
+    for(let i = 0; i < event.Records.length; i++){
+        const record = event.Records[i];
         if (record.eventName === 'INSERT') {
             const unMarshelledRecord = converter.unmarshall(record.dynamodb.NewImage);
 
@@ -20,4 +21,19 @@ export async function main(event, context) {
             }
         }
     }
+    // for (let record in event.Records) {
+    //     if (record.eventName === 'INSERT') {
+    //         const unMarshelledRecord = converter.unmarshall(record.dynamodb.NewImage);
+
+    //         console.log(unMarshelledRecord);
+    //         // send an email using SES
+    //         try {
+    //             console.log("sending email");
+    //             console.log(unMarshelledRecord);
+    //             await sesLib.call(unMarshelledRecord);
+    //         } catch (e) {
+    //             console.log("sending email failed");
+    //         }
+    //     }
+    // }
 }
